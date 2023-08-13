139adbc88dead6741adb57a75489fe8a00ac4f80
SaintJuniper <42616071+Jashior@users.noreply.github.com>
2023-08-13 19:31:32 +0100

    update csv

--- dict.js
+++ dict.js
@@ -83,7 +83,7 @@ axios
                 Team_2023_24: teamName,
               };
               console.log(
-                `New data: ${FPL_Name} (${web_name}, ${Team_2023_24}) (${minutes} mins).`
+                `New data: ${fplName} (${web_name}, ${teamName}) (${minutes} mins).`
               );
               existingData.push(newData);
             }
