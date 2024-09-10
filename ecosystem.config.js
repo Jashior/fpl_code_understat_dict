01bdea01597f685485bf1ae744dec33fa562a25c
SaintJuniper <42616071+Jashior@users.noreply.github.com>
2024-09-10 02:31:47 +0100

    startup fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,9 +9,6 @@ module.exports = {
       watch: true,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
         NODE_ENV: "production",
       },
     },
