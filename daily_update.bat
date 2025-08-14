71e6445d1c1483af3271c4e7dd342b52b8d980b5
SaintJuniper <42616071+Jashior@users.noreply.github.com>
2025-08-14 10:15:07 +0100

    chore: Remove daily_update.bat from tracking
    The daily_update.bat file is a local script and should not be tracked in the repository. This commit removes it from git tracking and adds it to the .gitignore file.

--- daily_update.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-echo "Running dict.js and updating git..."
-cd /D C:\Dev\Projects\2023\fpl_code_understat_dict
-node dict.js >> dict.log 2>&1
-git add .
-git commit -m "update dict"
-git push
-echo "Done."
